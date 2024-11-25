b251c9d0533bea2e20057ff7a6b8b8eb9c301c34
PopovaKateryna <kateryna.popova.it.2021@lpnu.ua>
2024-11-25 16:01:43 +0200

    add

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <!-- fOOD sEARCH Section Starts Here -->
     <section class="food-search text-center">
         <div class="container">
-            
+
             <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
                 <input type="search" name="search" placeholder="Search for Food.." required>
                 <input type="submit" name="submit" value="Search" class="btn btn-primary">
@@ -13,12 +13,11 @@
     </section>
     <!-- fOOD sEARCH Section Ends Here -->
 
-    <?php 
-        if(isset($_SESSION['order']))
-        {
-            echo $_SESSION['order'];
-            unset($_SESSION['order']);
-        }
+    <?php
+    if (isset($_SESSION['order'])) {
+        echo $_SESSION['order'];
+        unset($_SESSION['order']);
+    }
     ?>
 
     <!-- CAtegories Section Starts Here -->
@@ -26,56 +25,49 @@
         <div class="container">
             <h2 class="text-center">Explore Foods</h2>
 
-            <?php 
-                //Create SQL Query to Display CAtegories from Database
-                $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' LIMIT 3";
-                //Execute the Query
-                $res = mysqli_query($conn, $sql);
-                //Count rows to check whether the category is available or not
-                $count = mysqli_num_rows($res);
+            <?php
+            //Create SQL Query to Display CAtegories from Database
+            $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' LIMIT 3";
+            //Execute the Query
+            $res = mysqli_query($conn, $sql);
+            //Count rows to check whether the category is available or not
+            $count = mysqli_num_rows($res);
 
-                if($count>0)
-                {
-                    //CAtegories Available
-                    while($row=mysqli_fetch_assoc($res))
-                    {
-                        //Get the Values like id, title, image_name
-                        $id = $row['id'];
-                        $title = $row['title'];
-                        $image_name = $row['image_name'];
-                        ?>
-                        
-                        <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
-                            <div class="box-3 float-container">
-                                <?php 
-                                    //Check whether Image is available or not
-                                    if($image_name=="")
-                                    {
-                                        //Display MEssage
-                                        echo "<div class='error'>Image not Available</div>";
-                                    }
-                                    else
-                                    {
-                                        //Image Available
-                                        ?>
-                                        <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="Pizza" class="img-responsive img-curve">
-                                        <?php
-                                    }
-                                ?>
-                                
+            if ($count > 0) {
+                //CAtegories Available
+                while ($row = mysqli_fetch_assoc($res)) {
+                    //Get the Values like id, title, image_name
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $image_name = $row['image_name'];
+            ?>
 
-                                <h3 class="float-text text-white"><?php echo $title; ?></h3>
-                            </div>
-                        </a>
+                    <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
+                        <div class="box-3 float-container">
+                            <?php
+                            //Check whether Image is available or not
+                            if ($image_name == "") {
+                                //Display MEssage
+                                echo "<div class='error'>Image not Available</div>";
+                            } else {
+                                //Image Available
+                            ?>
+                                <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="Pizza" class="img-responsive img-curve">
+                            <?php
+                            }
+                            ?>
 
-                        <?php
-                    }
+
+                            <h3 class="float-text text-white"><?php echo $title; ?></h3>
+                        </div>
+                    </a>
+
+            <?php
                 }
-                else
-                {
-                    //Categories not Available
-                    echo "<div class='error'>Category not Added.</div>";
-                }
+            } else {
+                //Categories not Available
+                echo "<div class='error'>Category not Added.</div>";
+            }
             ?>
 
 
@@ -89,10 +81,10 @@
     <!-- fOOD MEnu Section Starts Here -->
     <section class="food-menu">
         <div class="container">
-            <h2 class="text-center">Food Menu</h2>
+            <h2 class="text-center">Food menu</h2>
 
-            <?php 
-            
+            <?php
+
             //Getting Foods from Database that are active and featured
             //SQL Query
             $sql2 = "SELECT * FROM tbl_food WHERE active='Yes' AND featured='Yes' LIMIT 6";
@@ -104,37 +96,32 @@
             $count2 = mysqli_num_rows($res2);
 
             //CHeck whether food available or not
-            if($count2>0)
-            {
+            if ($count2 > 0) {
                 //Food Available
-                while($row=mysqli_fetch_assoc($res2))
-                {
+                while ($row = mysqli_fetch_assoc($res2)) {
                     //Get all the values
                     $id = $row['id'];
                     $title = $row['title'];
                     $price = $row['price'];
                     $description = $row['description'];
                     $image_name = $row['image_name'];
-                    ?>
+            ?>
 
                     <div class="food-menu-box">
                         <div class="food-menu-img">
-                            <?php 
-                                //Check whether image available or not
-                                if($image_name=="")
-                                {
-                                    //Image not Available
-                                    echo "<div class='error'>Image not available.</div>";
-                                }
-                                else
-                                {
-                                    //Image Available
-                                    ?>
-                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                                    <?php
-                                }
+                            <?php
+                            //Check whether image available or not
+                            if ($image_name == "") {
+                                //Image not Available
+                                echo "<div class='error'>Image not available.</div>";
+                            } else {
+                                //Image Available
                             ?>
-                            
+                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                            <?php
+                            }
+                            ?>
+
                         </div>
 
                         <div class="food-menu-desc">
@@ -149,24 +136,22 @@
                         </div>
                     </div>
 
-                    <?php
+            <?php
                 }
-            }
-            else
-            {
+            } else {
                 //Food Not Available 
                 echo "<div class='error'>Food not available.</div>";
             }
-            
+
             ?>
 
-            
 
- 
+
+
 
             <div class="clearfix"></div>
 
-            
+
 
         </div>
 
@@ -176,5 +161,5 @@
     </section>
     <!-- fOOD Menu Section Ends Here -->
 
-    
+
     <?php include('partials-front/footer.php'); ?>
